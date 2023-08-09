362a8ad2dbc4dee4a63fc3dd8ce46d60fcfea844
scifisolutions <scifisolutions.in@gmail.com>
2023-08-09 11:29:49 +0530

    Post request added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+//import './App.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.post('http://localhost:8080/api/employee/test/list', {name:''})
+      .then(response => {
+        setPosts(response.data.payload);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            {post.id}, {post.name}, {post.salary}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
